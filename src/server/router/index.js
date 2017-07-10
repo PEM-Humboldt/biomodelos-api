46e86878d1486c842f5fb8a837198ba06ae3503f
Valentina Grajales <valegrajales@gmail.com>
2017-07-09 22:10:56 -0500

    Include OpenAPI UI for BioModelos API documentation

--- src/server/router/index.js
+++ src/server/router/index.js
@@ -1,4 +1,7 @@
 import fs from 'fs';
+import express from 'express';
+import favicon from 'serve-favicon';
+import path from 'path';
 import { config } from '../../config/application-config';
 
 export default app => {
@@ -7,5 +10,22 @@ export default app => {
       if (route.charAt(0) !== '_' && route.charAt(0) !== '.') {
         app.use(`/v2/${route}`, require(`../../api/${route}`).default);
       }
+  });
+
+  app.use(express.static(`${config.get('appSrc')}/views`));
+  app.use(favicon(path.join(config.get('appSrc'), 'views', 'favicon.ico')));
+
+  // Enable API documentation folder path
+  if (config.get('swagger.enabled'))
+    app.use('/api-doc', express.static(`${config.get('appRoot')}/api-doc`));
+
+  // Handle 404
+  app.use(function(req, res) {
+    res.status(404).sendFile(`${config.get('appSrc')}/views/404.html`);
+  });
+
+  // Handle 500
+  app.use(function(error, req, res, next) {
+    res.send('500: Internal Server Error', 500);
   });
 };
