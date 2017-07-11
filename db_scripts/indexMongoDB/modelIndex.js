ce8d7cf3638ae793aec34d8980bbfdfdb3988d29
Valentina Grajales <valegrajales@gmail.com>
2017-07-11 09:00:52 -0500

    Update index scripts

--- db_scripts/indexMongoDB/modelIndex.js
+++ db_scripts/indexMongoDB/modelIndex.js
@@ -1,7 +1,15 @@
 //DB connection
-var db = connect('localhost:27017/records');//localhost:27017/records
+var db = connect('localhost:27017/records'); //localhost:27017/records
 
 //creating indexes for records.models collection
-db.models.createIndex({"taxID": 1});
-db.models.createIndex({"taxID": 1, "modelStatus": 1});
-db.models.createIndex({"modelID": 1});
+db.models.createIndex({ taxID: 1 });
+db.models.createIndex({ taxID: 1, modelStatus: 1 });
+db.models.createIndex({ modelID: 1 });
+db.models.createIndex({
+  taxID: 1,
+  isActive: 1,
+  modelStatus: 1,
+  thresholdType: 1,
+  published: 1,
+  modelID: 1
+});
--- db_scripts/indexMongoDB/recordsIndex.js
+++ db_scripts/indexMongoDB/recordsIndex.js
@@ -1,10 +1,19 @@
 //DB connection
-var db = connect('localhost:27017/records');//localhost:27017/records
+var db = connect('localhost:27017/records'); //localhost:27017/records
 
 //creating indexes for records.records collection
-db.records.createIndex({"taxID": 1});
-db.records.createIndex({"taxID": 1, "reported": 1, "updated": 1});
-db.records.createIndex({"taxID": 1, "created": 1});
-db.records.createIndex({"taxID": 1, "use": 1, "visualizationPrivileges": 1});
-db.records.createIndex({"taxID": 1, "institution": 1});
-db.records.createIndex({"taxID": 1, "collector": 1});
+db.records.createIndex({ taxID: 1 });
+db.records.createIndex({ taxID: 1, reported: 1, updated: 1 });
+db.records.createIndex({ taxID: 1, created: 1 });
+db.records.createIndex({ taxID: 1, use: 1, visualizationPrivileges: 1 });
+db.records.createIndex({ taxID: 1, institution: 1 });
+db.records.createIndex({ taxID: 1, collector: 1 });
+db.records.createIndex({
+  taxID: 1,
+  use: 1,
+  visualizationPrivileges: 1,
+  spatialDuplicated: 1,
+  species: 1,
+  collector: 1,
+  source: 1
+});
