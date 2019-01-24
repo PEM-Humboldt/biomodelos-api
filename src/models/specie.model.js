338392d1e06000cafd67d9efc5f083658020ba64
Erika Suárez Valencia <marvinesv@gmail.com>
2019-01-24 09:30:18 -0500

    update species model schema according to the dictionary

--- src/models/specie.model.js
+++ src/models/specie.model.js
@@ -41,7 +41,8 @@ const SpecieSchema = new Schema(
         'I/II/III/NC',
         'I/II/NC'
       ],
-      default: ''
+      default: '',
+      required: true
     },
     endemic: { type: Boolean, default: false, required: true },
     invasive: { type: Boolean, default: false, required: true },
@@ -62,7 +63,8 @@ const SpecieSchema = new Schema(
       type: String,
       required: true,
       in: ['EX', 'EW', 'CR', 'EN', 'VU', 'NT', 'LC', 'DD', 'NE'],
-      default: 'NE'
+      default: 'NE',
+      required: true
     },
     taxVerifSource: {
       type: String,
@@ -72,25 +74,38 @@ const SpecieSchema = new Schema(
     nameAccordingTo: { type: String, default: '' },
     scientificNameAuthorship: { type: String, default: '' },
     specificEpithet: { type: String, default: null, required: true },
-    taxonomicStatus: { type: String, default: null },
+    taxonomicStatus: {
+      type: String,
+      in: [
+        'accepted',
+        'valid',
+        'synonym',
+        'homotypic synonym',
+        'heterotypic synonym',
+        'proParteSynonym',
+        'misapplied',
+        'pending',
+        'provisionally accepted name',
+        ''
+      ],
+      default: ''
+    },
     validName: { type: Boolean, default: true, required: true },
     speciesInCountry: { type: Boolean, default: null, required: true },
     sppInCol: { type: Boolean, default: true, required: true },
     iucnSpeciesID: { type: Number, default: 0 },
     amenazaNacional: {
       type: String,
-      required: true,
       in: ['EX', 'EW', 'CR', 'EN', 'VU', 'NT', 'LC', 'DD', 'NE'],
+      required: true,
       default: 'NE'
     },
     recordsVisualizationPrivileges: {
       type: Number,
-      required: true,
       in: [0, 1, 2],
+      required: true,
       default: 0
-    },
-    uploadUser: { type: String, required: true, default: null },
-    uploadDate: { type: Date, required: true, default: null }
+    }
   },
   { collection: 'species' }
 );
