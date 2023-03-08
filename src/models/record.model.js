8530077ed799e4cd07f8a8e16ea624f0ec208219
Erika Suárez Valencia <1734998+erikasv@users.noreply.github.com>
2023-03-08 18:16:56 -0500

    Add validations for null and NULL strings in all string fields

--- src/models/record.model.js
+++ src/models/record.model.js
@@ -22,43 +22,237 @@ const RecordSchema = new Schema(
     occurrenceID: { type: String, default: `Biomodelos-${Date.now()}` }, // TODO: Is mandatory according to calc file
     taxID: { type: Number, required: true },
     acceptedNameUsage: { type: String, required: true },
-    species: { type: String, default: '' },
-    speciesOriginal: { type: String, default: '' },
-    stateProvince: { type: String, default: '' },
-    county: { type: String, default: '' },
-    verbatimLocality: { type: String, default: null }, // TODO in the database rules this field can't be null
+    species: {
+      type: String,
+      default: null,
+      validate: {
+        validator: val => {
+          if (val.toLowerCase().trim() == 'null') return false;
+        },
+        message: 'validation of `{PATH}` failed with value "{VALUE}" (string)'
+      }
+    },
+    speciesOriginal: {
+      type: String,
+      default: null,
+      validate: {
+        validator: val => {
+          if (val.toLowerCase().trim() == 'null') return false;
+        },
+        message: 'validation of `{PATH}` failed with value "{VALUE}" (string)'
+      }
+    },
+    stateProvince: {
+      type: String,
+      default: null,
+      validate: {
+        validator: val => {
+          if (val.toLowerCase().trim() == 'null') return false;
+        },
+        message: 'validation of `{PATH}` failed with value "{VALUE}" (string)'
+      }
+    },
+    country: {
+      type: String,
+      default: null,
+      validate: {
+        validator: val => {
+          if (val.toLowerCase().trim() == 'null') return false;
+        },
+        message: 'validation of `{PATH}` failed with value "{VALUE}" (string)'
+      }
+    },
+    county: {
+      type: String,
+      default: null,
+      validate: {
+        validator: val => {
+          if (val.toLowerCase().trim() == 'null') return false;
+        },
+        message: 'validation of `{PATH}` failed with value "{VALUE}" (string)'
+      }
+    },
+    eventDate: {
+      type: String,
+      default: new Date().toISOString().slice(0, 10),
+      validate: {
+        validator: val => {
+          if (val.toLowerCase().trim() == 'null') return false;
+        },
+        message: 'validation of `{PATH}` failed with value "{VALUE}" (string)'
+      }
+    },
+    verbatimLocality: {
+      type: String,
+      default: null,
+      validate: {
+        validator: val => {
+          if (val.toLowerCase().trim() == 'null') return false;
+        },
+        message: 'validation of `{PATH}` failed with value "{VALUE}" (string)'
+      }
+    },
     decimalLatitude: { type: Number, required: true, min: -90, max: 90 },
     decimalLongitude: { type: Number, required: true, min: -180, max: 180 },
-    verbatimElevation: { type: String, default: null },
+    verbatimElevation: {
+      type: String,
+      default: null,
+      validate: {
+        validator: val => {
+          if (val.toLowerCase().trim() == 'null') return false;
+        },
+        message: 'validation of `{PATH}` failed with value "{VALUE}" (string)'
+      }
+    },
     cellID: { type: Number, min: 0, default: null },
-    basisOfRecord: { type: String, default: null },
-    catalogNumber: { type: String, default: null },
-    recordedBy: { type: String, default: null },
-    institutionCode: { type: String, default: null },
-    url: { type: String, default: null },
+    basisOfRecord: {
+      type: String,
+      default: null,
+      validate: {
+        validator: val => {
+          if (val.toLowerCase().trim() == 'null') return false;
+        },
+        message: 'validation of `{PATH}` failed with value "{VALUE}" (string)'
+      }
+    },
+    catalogNumber: {
+      type: String,
+      default: null,
+      validate: {
+        validator: val => {
+          if (val.toLowerCase().trim() == 'null') return false;
+        },
+        message: 'validation of `{PATH}` failed with value "{VALUE}" (string)'
+      }
+    },
+    recordedBy: {
+      type: String,
+      default: null,
+      validate: {
+        validator: val => {
+          if (val.toLowerCase().trim() == 'null') return false;
+        },
+        message: 'validation of `{PATH}` failed with value "{VALUE}" (string)'
+      }
+    },
+    institutionCode: {
+      type: String,
+      default: null,
+      validate: {
+        validator: val => {
+          if (val.toLowerCase().trim() == 'null') return false;
+        },
+        message: 'validation of `{PATH}` failed with value "{VALUE}" (string)'
+      }
+    },
+    url: {
+      type: String,
+      default: null,
+      validate: {
+        validator: val => {
+          if (val.toLowerCase().trim() == 'null') return false;
+        },
+        message: 'validation of `{PATH}` failed with value "{VALUE}" (string)'
+      }
+    },
     day: { type: Number, min: 1, max: 31, default: null },
     month: { type: Number, min: 1, max: 12, default: null },
     year: { type: Number, min: 1800, max: 2100, default: null },
     inUrbanArea: { type: Boolean, default: null },
-    suggestedCounty: { type: String, default: null },
-    suggestedStateProvince: { type: String, default: null },
-    sourceLayer: { type: String, default: null },
+    suggestedCounty: {
+      type: String,
+      default: null,
+      validate: {
+        validator: val => {
+          if (val.toLowerCase().trim() == 'null') return false;
+        },
+        message: 'validation of `{PATH}` failed with value "{VALUE}" (string)'
+      }
+    },
+    suggestedStateProvince: {
+      type: String,
+      default: null,
+      validate: {
+        validator: val => {
+          if (val.toLowerCase().trim() == 'null') return false;
+        },
+        message: 'validation of `{PATH}` failed with value "{VALUE}" (string)'
+      }
+    },
+    sourceLayer: {
+      type: String,
+      default: null,
+      validate: {
+        validator: val => {
+          if (val.toLowerCase().trim() == 'null') return false;
+        },
+        message: 'validation of `{PATH}` failed with value "{VALUE}" (string)'
+      }
+    },
     environmentalOutlier: { type: Boolean, default: null },
     dbDuplicate: { type: Boolean, default: false },
     spatialDuplicated: { type: Boolean, default: false },
     updated: [UpdatedSchema],
     downloadDate: {
       type: String,
-      default: new Date().toISOString().slice(0, 10)
+      default: new Date().toISOString().slice(0, 10),
+      validate: {
+        validator: val => {
+          if (val.toLowerCase().trim() == 'null') return false;
+        },
+        message: 'validation of `{PATH}` failed with value "{VALUE}" (string)'
+      }
     },
-    resourceFolder: { type: String, default: 'Biomodelos' }, // TODO: Is mandatory according to calc file
+    resourceFolder: {
+      type: String,
+      default: 'Biomodelos',
+      validate: {
+        validator: val => {
+          if (val.toLowerCase().trim() == 'null') return false;
+        },
+        message: 'validation of `{PATH}` failed with value "{VALUE}" (string)'
+      }
+    },
     resourceIncorporationDate: {
       type: String,
-      default: Date.now()
-    }, // TODO: Is mandatory according to calc file
-    resourceName: { type: String, default: 'Biomodelos' }, // TODO: Is mandatory according to calc file
-    source: { type: String, default: null },
-    contributedRecord: { type: String, default: null },
+      default: Date.now(),
+      validate: {
+        validator: val => {
+          if (val.toLowerCase().trim() == 'null') return false;
+        },
+        message: 'validation of `{PATH}` failed with value "{VALUE}" (string)'
+      }
+    },
+    resourceName: {
+      type: String,
+      default: 'Biomodelos',
+      validate: {
+        validator: val => {
+          if (val.toLowerCase().trim() == 'null') return false;
+        },
+        message: 'validation of `{PATH}` failed with value "{VALUE}" (string)'
+      }
+    },
+    source: {
+      type: String,
+      default: null,
+      validate: {
+        validator: val => {
+          if (val.toLowerCase().trim() == 'null') return false;
+        },
+        message: 'validation of `{PATH}` failed with value "{VALUE}" (string)'
+      }
+    },
+    contributedRecord: {
+      type: String,
+      default: null,
+      validate: {
+        validator: val => {
+          if (val.toLowerCase().trim() == 'null') return false;
+        },
+        message: 'validation of `{PATH}` failed with value "{VALUE}" (string)'
+      }
+    },
     privateData: { type: Number, required: true, in: [0, 1, 2], default: 0 },
     use: { type: Boolean, default: true },
     visualizationPrivileges: {
@@ -67,7 +261,16 @@ const RecordSchema = new Schema(
       in: [0, 1, 2],
       default: 0
     },
-    collectionCode: { type: String, default: null },
+    collectionCode: {
+      type: String,
+      default: null,
+      validate: {
+        validator: val => {
+          if (val.toLowerCase().trim() == 'null') return false;
+        },
+        message: 'validation of `{PATH}` failed with value "{VALUE}" (string)'
+      }
+    },
     userIdBm: { type: Number },
     // Created fields
     createdCommentsBm: { type: String },
